30577fb832706bd2cfa7ce5343980eacdcfe9b24
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2026-03-16 19:44:46 +0700

    fix spa app static routing on github page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Certificates from "./pages/Certificates";
 
 const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   [
     {
       path: "/",
